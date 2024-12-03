87222fa6a1e897467d783388b5739dd1c047f58f
EliorAvisrur <elior261200@gmail.com>
2024-12-03 11:50:35 +0200

    fix data problem from mongo

--- server/controllers/CategoriesController.js
+++ server/controllers/CategoriesController.js
@@ -2,7 +2,7 @@ import Category from "../models/categoryModel.js";
 const controller = {
   async getAll(req, res, next) {
     try {
-      let {data} = await Category.find();
+      let data = await Category.find();
       res.json(data);
     } catch (err) {
       res.json({ error: err });
