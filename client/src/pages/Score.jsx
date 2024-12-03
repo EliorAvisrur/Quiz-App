61a8ca1511aa644ed85ad78609c322f6cecdf6a8
EliorAvisrur <elior261200@gmail.com>
2024-12-03 12:12:59 +0200

    update score calculating

--- client/src/pages/Score.jsx
+++ client/src/pages/Score.jsx
@@ -5,17 +5,19 @@ import { useNavigate } from "react-router-dom"
 const Score = () => {
   const { answers, setAnswers } = useAnswersContext()
   console.log(answers)
-  const [score, setScore] = useState(0)
+  const [scorePoints, setScorePoints] = useState(0)
+  const [scorePercentage, setScorePercentage] = useState(0)
   const nav=useNavigate()
   useEffect(() => {
-    let s = 0
+    let points=0 ;
     const calculateScore = () => {
       answers.forEach((item) => {
         if (Number(item.value) == item.answer) {
-          s += 10
+          points+=1
         }
       })
-      setScore(s)
+      setScorePoints((points / answers.length).toFixed(3)*100)
+      setScorePercentage((points / answers.length).toFixed(3) * 100)
     }
     calculateScore()
   }, [])
@@ -29,17 +31,21 @@ const Score = () => {
       </h2>
       <div className="mb-3 flex items-center justify-around">
         <div className="flex h-28 w-28 flex-col items-center justify-center rounded-full bg-gradient-to-l from-indigo-500 via-purple-600 to-pink-500">
-          <h3 className="text-pretty text-3xl font-bold text-white">{score}</h3>
+          <h3 className="text-pretty text-3xl font-bold text-white">
+            {scorePoints}
+          </h3>
           <p className="text-lg text-white">Points</p>
         </div>
         <div className="flex h-28 w-28 flex-col items-center justify-center rounded-full bg-gradient-to-l from-indigo-500 via-purple-600 to-pink-500">
-          <h3 className="text-pretty text-3xl font-bold text-white">{score}%</h3>
+          <h3 className="text-pretty text-3xl font-bold text-white">
+            {scorePercentage}%
+          </h3>
           <p className="text-lg text-white">Percentage</p>
         </div>
       </div>
       <div className="text-center">
         <button
-          className="mt-3 rounded-3xl bg-indigo-300 bg-opacity-50 p-3 shadow-2xl shadow-gray-500 font-semibold text-white hover:scale-105 ease-in-out focus:scale-100"
+          className="mt-3 rounded-3xl bg-indigo-300 bg-opacity-50 p-3 font-semibold text-white shadow-2xl shadow-gray-500 ease-in-out hover:scale-105 focus:scale-100"
           onClick={backToHome}
         >
           Back to home page
